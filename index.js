785cb9a3102de27d9d89e1a75f042e441e45cf84
Ados4453 <matthawkins44@gmail.com>
2022-09-20 01:51:03 -0500

    did a test call to the front end to make sure it worked

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ app.use(express.json()); // to get request body
 app.use(express.static('public')); // to get images as 
 
 //Routes
-app.use("/");
+app.use("/", (req,res) =>{
+    res.json({"users":["userOne","userTwo", "userThree"]})
+});
 
 app.listen(8080, () =>{
-    // console.log(`Running on port ${PORT}....`)
+    console.log(`Running on port ${PORT}....`)
     console.log('To kill API press CTRL + C')
+    console.log('Or dont.. its not like i can go anywhere else >:(')
 })
